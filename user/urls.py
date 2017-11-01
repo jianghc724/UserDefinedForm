b54a7975fcf9bfb58cf4081357813e3b60f6901f
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2017-11-01 17:49:11 +0800

    backend structure and config example for server

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,9 @@ from user.views import *
 urlpatterns = [
     # url(r'^user/register/?$', UserRegister.as_view()),
     url(r'^login/?$', UserLogin.as_view()),
+    url(r'^apply/list?$', ApplyList.as_view()),
+    url(r'^question/?$', CreateQuestion.as_view()),
+    url(r'^section/?$', CreateSection.as_view()),
+    url(r'^form/?$', CreateForm.as_view()),
+    url(r'^rate/?$', HandleApply.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from codex.baseerror import *
 from codex.baseview import APIView
 from user.models import *
+from django.contrib.auth import authenticate,login,logout
 
 import requests
 import json
@@ -9,14 +10,60 @@ import html
 from datetime import datetime
 
 # Create your views here.
+
+
 class UserLogin(APIView):
+    def get(self):
+        if self.request.user.is_authenticated():
+            return
+        else:
+            raise LogicError("")
+
+    def post(self):
+        self.check_input('user_id', 'password')
+        user = authenticate(username=self.input['user_id'], password=self.input['password'])
+        if user is not None:
+            if user.is_active:
+                login(self.request, user)
+                return
+        raise ValidateError('username or password wrong')
+
+
+class ApplyList(APIView):
     def get(self):
         pass
 
     def post(self):
-        # to do
-        self.check_input('user_id', 'password')
-        user = User.objects.get(user=self.input['user_id'])
+        pass
 
 
-        raise ValidateError('username or password wrong')
+class CreateQuestion(APIView):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class CreateSection(APIView):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class CreateForm(APIView):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+
+class HandleApply(APIView):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
